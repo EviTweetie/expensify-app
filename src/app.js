209a454f8d8bd0f0d6290f13679cbd036566e2f4
EviTweetie <EviTweetie@gmail.com>
2018-08-13 12:09:19 +0200

    11.104 SortBy Date or Amount - Selection (Challenge)

--- src/app.js
+++ src/app.js
@@ -11,37 +11,21 @@ import './styles/styles.scss'
 
 const store = configureStore()
 
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }))
 store.dispatch(
-  addExpense({ description: 'Water Bill', amount: 15000, createdAt: 200 })
-)
-store.dispatch(
-  addExpense({ description: 'Gas Bill', amount: 12500, createdAt: 500 })
+  addExpense({ description: 'Gas Bill', amount: 1000, createdAt: 1000 })
 )
 
-store.dispatch(setStartDate(125))
-store.dispatch(setEndDate(1250))
-store.dispatch(setTextFilter('water'))
-
-setTimeout(() => {
-  store.dispatch(
-    addExpense({ description: 'Restaurant Bill', amount: 1000, createdAt: 400 })
-  )
-}, 2000)
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('ill'))
-}, 3000)
+store.dispatch(addExpense({ description: 'Rent', amount: 10950 }))
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 )
-
-// console.log('STORE', store)
-// console.log('STATE', state)
 
 ReactDOM.render(jsx, document.getElementById('app'))
