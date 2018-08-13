f6136b892bfd492002d57efc61db77030ca84770
EviTweetie <EviTweetie@gmail.com>
2018-08-13 12:20:11 +0200

    11.104 SortBy: Controlled Input

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -18,9 +18,11 @@ const ExpenseListFilters = props => {
         value={props.filters.sortBy}
         onChange={e => {
           console.log(e.target.value)
-          e.target.value === 'date'
-            ? props.dispatch(sortByDate())
-            : props.dispatch(sortByAmount())
+          if (e.target.value === 'date') {
+            props.dispatch(sortByDate())
+          } else if (e.target.value === 'amount') {
+            props.dispatch(sortByAmount())
+          }
         }}
       >
         <option value="date">Date</option>
