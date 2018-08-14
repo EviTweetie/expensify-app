edb871c3556694cefeb6f8871c8ce9c66a78c06f
EviTweetie <EviTweetie@gmail.com>
2018-08-14 11:50:30 +0200

    11.107 Create Expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,20 +3,14 @@ import moment from 'moment'
 import { SingleDatePicker } from 'react-dates'
 import 'react-dates/lib/css/_datepicker.css'
 
-// const date = new Date()
-// console.log(date)
-
-// const now = moment()
-// console.log(now.format('DD.MM.YYYY'))
-// console.log(now)
-
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     amount: '',
     note: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   }
   onDescriptionChange = e => {
     const description = e.target.value
@@ -28,21 +22,51 @@ export default class ExpenseForm extends React.Component {
   }
   onAmountChange = e => {
     const amount = e.target.value
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }))
     }
   }
   onDateChange = createdAt => {
-    this.setState(() => ({ createdAt }))
+    if (createdAt) {
+      this.setState(() => ({ createdAt }))
+    }
+    //if: tweak to unable deleting the date value as a better user experience
   }
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }))
+  }
+  onSubmit = e => {
+    e.preventDefault()
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({
+        error: 'Please provide a description and amount.'
+      }))
+    } else {
+      this.setState(() => ({
+        error: ''
+      }))
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      })
+    }
   }
   render() {
     return (
       <div>
         <h1>Expense Form</h1>
-        <form>
+        <span if="ERRORAREA" style={{ color: 'red' }}>
+          {!!this.state.error ? this.state.error : null}
+        </span>
+        {this.state.error && (
+          <p>
+            Andrew's solution (logical AND):
+            <span style={{ color: 'orange' }}>{this.state.error}</span>
+          </p>
+        )}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
