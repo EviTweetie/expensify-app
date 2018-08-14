89690a04dc04ec2d62842a3ddbd69ae67faf358e
EviTweetie <EviTweetie@gmail.com>
2018-08-14 13:36:31 +0200

    11.108 put remove button into EditExpense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,32 +1,15 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { removeExpense } from '../actions/expenses'
 import { Link } from 'react-router-dom'
 
-const ExpenseListItem = ({
-  description,
-  amount,
-  createdAt,
-  id,
-  dispatch,
-  history
-}) => (
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
     <p>
-      {amount} - {createdAt}{' '}
-      <button
-        onClick={e => {
-          console.log(e, id)
-          dispatch(removeExpense({ id }))
-        }}
-      >
-        Remove
-      </button>
+      {amount} - {createdAt}
     </p>
   </div>
 )
 
-export default connect()(ExpenseListItem)
+export default ExpenseListItem
