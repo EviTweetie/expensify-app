b01e5fbb1b1c20a73e6620f28ca70537f94be2a7
EviTweetie <EviTweetie@gmail.com>
2018-08-16 22:20:10 +0200

    12.112 use built-in-assertion: expect

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -2,16 +2,19 @@
 //Test Example
 const add = (a, b) => {
   return a + b
+  //check for errors
+  //return a + b + 1
 }
 
 //New Test Case
-// test('string arg1',() => {
+// test('string arg1s',() => {
 
 // })
 test('should add two numbers', () => {
   const result = add(3, 4)
   //Make Assertions
-  if (result !== 7) {
-    throw new Error(`You added 3 and 4. The result was ${result}. Expexted 7!`)
-  }
+  //   if (result !== 7) {
+  //     throw new Error(`You added 3 and 4. The result was ${result}. Expexted 7!`)
+  //   }
+  expect(result).toBe(7)
 })
