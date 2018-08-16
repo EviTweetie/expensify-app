feac34b0b41052cc88c00ee4aaa97a3560e43eb7
EviTweetie <EviTweetie@gmail.com>
2018-08-16 22:02:39 +0200

    12.112 First Test file add.test.js

---
+++ src/tests/add.test.js
@@ -0,0 +1,17 @@
+//file.test.js jest needs this naming convention
+//Test Example
+const add = (a, b) => {
+  return a + b
+}
+
+//New Test Case
+// test('string arg1',() => {
+
+// })
+test('should add two numbers', () => {
+  const result = add(3, 4)
+  //Make Assertions
+  if (result !== 7) {
+    throw new Error(`You added 3 and 4. The result was ${result}. Expexted 7!`)
+  }
+})
