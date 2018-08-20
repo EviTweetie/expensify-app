7757dcd5eaccc11cfc1d55fef963632e5e8970eb
EviTweetie <EviTweetie@gmail.com>
2018-08-20 22:00:38 +0200

    12.113 testing expense actions generators addExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -26,3 +26,53 @@ test('should setup edit expense action object', () => {
     }
   })
 })
+
+test('should setup addExpense action object with provided values', () => {
+  const expenseData = {
+    description: 'Rent',
+    amount: 109500,
+    createdAt: 1000,
+    note: 'This was last months rent'
+  }
+  const action = addExpense(expenseData)
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  })
+})
+
+test('should setup addExpense action object with provided values', () => {
+  const expenseDataDefault = {
+    description: '',
+    note: '',
+    amount: 0,
+    createdAt: 0
+  }
+  const action = addExpense()
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseDataDefault,
+      id: expect.any(String)
+    }
+  })
+})
+//Andrews solution to challenge
+/*
+test('should setup addExpense action object with provided values', () => {
+  const action = addExpense()
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: expect.any(String),
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0
+    }
+  })
+})
+*/
